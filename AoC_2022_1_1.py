bc4254cba9fac54e59fb33b613dad25f4a0a6f49
tilley14 <nmt2400@g.rit.edu>
2022-12-01 09:20:57 -0500

    Create AoC_2022_1_1.py

---
+++ AoC_2022_1_1.py
@@ -0,0 +1,20 @@
+# Advent of Code 2022, Day 1, Puzzle 1
+
+cals = []  # To Do Quick Sanity Check
+maxCal = 0
+with open("cal.txt") as f:
+    sum = 0
+    for line in f.readlines():
+        line = line.strip()
+        if line:
+            sum = sum + int(line)
+        else:
+            maxCal = max(sum, maxCal)
+            cals.append(sum)
+            sum = 0
+
+    maxCal = max(sum, maxCal)
+    cals.append(sum)
+
+print(cals)
+print(maxCal)
