235f8dc0fa63593f7d2c6864c1ee2ecaf93554da
Nikolas Tilley <nmt2400@g.rit.edu>
2022-12-06 00:23:16 -0500

    removeing debug

--- day6/AoC_2022_6_1.py
+++ day6/AoC_2022_6_1.py
@@ -5,7 +5,6 @@ endOfPacket = 0
 with open("input.txt") as f:
     line = f.readline()
     for x in range(len(line) - 4):
-        print(set(line[x:x+4]))
         if len(set(line[x:x+4])) == 4:
             endOfPacket =  x+4
             break
--- day6/AoC_2022_6_2.py
+++ day6/AoC_2022_6_2.py
@@ -5,7 +5,6 @@ endOfPacket = 0
 with open("input.txt") as f:
     line = f.readline()
     for x in range(len(line) - 14):
-        # print(set(line[x:x+14]))
         if len(set(line[x:x+14])) == 14:
             endOfPacket =  x+14
             break
