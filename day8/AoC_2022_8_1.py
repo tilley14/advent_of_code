0a54dfdd484c50ffb27a6d01ef18d1341a9b3395
Nikolas Tilley <nmt2400@g.rit.edu>
2022-12-08 01:04:33 -0500

    Day8

--- day8/AoC_2022_8_1.py
+++ day8/AoC_2022_8_1.py
@@ -2,9 +2,32 @@
 
 ret = 0
 
+
+grid = []
 with open("input.txt") as f:
-    for line in f.readlines():
-        pass
-        
+    grid = [[t for t in r.strip()] for r in f.readlines() if r.strip()]
+
+rows = len(grid)
+cols = len(grid[0])
+
+ret = ((rows + cols) * 2) - 4 # perimeter trees
+
+def isVisible(r, c):
+    global grid
+    val = grid[r][c]
+    
+    top = max([grid[rr][c] for rr in range(0, r)])
+    bot = max([grid[rr][c] for rr in range(r+1, rows)])
+    left = max([grid[r][cc] for cc in range(0, c)])
+    right = max([grid[r][cc] for cc in range(c+1, cols)])
+    
+    return val > min(top, bot, left, right)
+
+
+for r in range(1, len(grid) - 1):
+    for c in range (1, len(grid[0]) - 1):
+        if isVisible(r,c):
+            ret += 1
+
 
 print(ret)
--- day8/AoC_2022_8_2.py
+++ day8/AoC_2022_8_2.py
@@ -2,9 +2,51 @@
 
 ret = 0
 
-with open("input.txt") as f:
-    for line in f.readlines():
-        pass
-        
+grid = []
 
-print(ret)
+with open("input.txt") as f:
+    grid = [[t for t in r.strip()] for r in f.readlines() if r.strip()]
+
+rows = len(grid)
+cols = len(grid[0])
+
+
+def scenicScore(r, c):
+    global grid
+    val = grid[r][c]
+
+    top = 0
+    bot = 0
+    left = 0
+    right = 0
+
+    for v in [grid[rr][c] for rr in range(0, r)][::-1]:
+        top += 1
+        if v >= val:
+            break
+
+    for v in [grid[rr][c] for rr in range(r + 1, rows)]:
+        bot += 1
+        if v >= val:
+            break
+
+    for v in [grid[r][cc] for cc in range(0, c)][::-1]:
+        left += 1
+        if v >= val:
+            break
+
+    for v in [grid[r][cc] for cc in range(c + 1, cols)]:
+        right += 1
+        if v >= val:
+            break
+
+    return top * bot * left * right
+
+
+scores = []
+for r in range(0, len(grid)):
+    for c in range(0, len(grid[0])):
+        scores.append(scenicScore(r, c))
+
+
+print(max(scores))
