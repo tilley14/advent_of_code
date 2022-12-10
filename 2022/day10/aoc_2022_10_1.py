5e53a2131ac4e773ca868baa8f1aae8cbd7f36e9
Nikolas Tilley <nmt2400@g.rit.edu>
2022-12-10 01:31:56 -0500

    day10 solution

--- 2022/day10/aoc_2022_10_1.py
+++ 2022/day10/aoc_2022_10_1.py
@@ -1,9 +1,46 @@
 # Advent of Code 2022, Day 10, Puzzle 1
 
-ret = 0
+CYCLES = 0
+X = 1
+SIG_STRENGTH = []
+
+
+def sigStr():
+    return CYCLES * X
+
+
+def incrementCycles():
+    global CYCLES
+    CYCLES += 1
+    if (CYCLES % 20 == 0):
+        print("c{} x{} ss {}".format(CYCLES, X, sigStr()))
+        SIG_STRENGTH.append(sigStr())
+
+
+def addV(v):
+    global x
+    incrementCycles()
+    incrementCycles()
+    X += v
+
+
+def noop():
+    incrementCycles()
+
+
+INPUT = []
 
 with open("input.txt") as f:
-    for line in f.readlines():
-        pass
+    INPUT = [l.strip() for l in f.read().splitlines() if l.strip()]
 
-print(ret)
+
+for l in INPUT:
+    instruction = l.split(" ")
+    if (instruction[0] == "noop"):
+        noop()
+    elif instruction[0] == "addx":
+        addV(int(instruction[1]))
+
+
+print(sum(SIG_STRENGTH[0:12:2]))
+print(len(SIG_STRENGTH[0:12:2]))
--- 2022/day10/aoc_2022_10_2.py
+++ 2022/day10/aoc_2022_10_2.py
@@ -1,9 +1,58 @@
 # Advent of Code 2022, Day 10, Puzzle 2
 
-ret = 0
+CYCLES = 0
+X = 1
+CTR_W = 40
+CTR_H = 6
+PIXES = ["" for i in range(CTR_H*CTR_W)]
+LIT = "#"
+DARK = "."
+
+
+def overlaps():
+    return CYCLES % CTR_W in [X-1, X, X+1]
+
+
+def drawPix():
+    PIXES[CYCLES] = LIT if overlaps() else DARK
+
+
+def incrementCycles():
+    global CYCLES
+    drawPix()
+    CYCLES += 1
+
+
+def addV(v):
+    global X
+    incrementCycles()
+    incrementCycles()
+    X += v
+
+
+def noop():
+    incrementCycles()
+
+
+INPUT = []
 
 with open("input.txt") as f:
-    for line in f.readlines():
-        pass
+    INPUT = [l.strip() for l in f.read().splitlines() if l.strip()]
 
-print(ret)
+
+for l in INPUT:
+    instruction = l.split(" ")
+    if (instruction[0] == "noop"):
+        noop()
+    elif instruction[0] == "addx":
+        addV(int(instruction[1]))
+
+
+img = [["" for i in range(CTR_W)] for i in range(CTR_H)]
+x = y = 0
+for i in range(0, len(PIXES)):
+    x = i % CTR_W
+    y = i // CTR_W
+    img[y][x] = PIXES[i]
+
+print("\n".join("".join(r) for r in img))
